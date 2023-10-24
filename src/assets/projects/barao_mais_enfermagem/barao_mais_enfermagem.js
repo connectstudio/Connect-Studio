ce5986b62bc6b490d5a56fb50e01f2c7f9de779a
connectstudio <connectstudiohd@gmail.com>
2023-10-24 15:22:26 -0300

    Cases page added

---
+++ src/assets/projects/barao_mais_enfermagem/barao_mais_enfermagem.js
@@ -0,0 +1,17 @@
+import bme from './barao_mais_enfermagem.png';
+import bme_1 from './barao_mais_enfermagem_1.jpeg';
+import bme_2 from './barao_mais_enfermagem_2.jpeg';
+import bme_3 from './barao_mais_enfermagem_3.jpeg';
+import bme_4 from './barao_mais_enfermagem_4.jpeg';
+
+const bme_data = {
+    thumb: bme.src,
+    images: [
+        {src: bme_1.src},
+        {src: bme_2.src},
+        {src: bme_3.src},
+        {src: bme_4.src}
+    ]
+}
+
+export default bme_data
